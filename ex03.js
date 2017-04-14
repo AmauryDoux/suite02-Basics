aeb72b0de7436e6ad8c53ea85fa281d584dc7ca1
AmauryDoux <amaury.doux@gmail.com>
2017-04-14 15:46:50 +0200

    exercice 3 finished

--- ex03.js
+++ ex03.js
@@ -13,3 +13,14 @@
         copyArray(["bonjour", 4, 5]); // ["bonjour", 4, 5]
 
 */
+
+
+function copyArray(arr) {
+    var len = arr.length;
+    var arr2 = [];
+    for (var i = 0; i < len; i++) {
+
+        arr2.push(arr[i])
+    }
+    return arr2;
+}
